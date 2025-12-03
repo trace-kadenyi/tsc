3c233f19b35d6581402970df2f1c97b1ccebf81d
Tracey Kadenyi <treykadenyi@gmail.com>
2025-12-03 16:08:14 +0300

    functions

--- src/main.ts
+++ src/main.ts
@@ -17,3 +17,20 @@ let myName: "Tracey Kadenyi";
 // strictly only the listed names
 let userName: "Trace" | "Bruce" | "Bruno" | "Colleta";
 userName = "Bruce";
+
+// functions
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+// console.log(add(4, 5));
+
+// no return
+
+const logMsg = (message: any): void => {
+  console.log(message);
+};
+
+
+logMsg('Hello!')
+logMsg(add(2, 3))
