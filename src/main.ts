dd24b8b2fd6553ae288164d752095c1a12b5e0fe
Tracey Kadenyi <treykadenyi@gmail.com>
2025-12-03 15:14:09 +0300

    use type aliases

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,12 @@
-// let stringArr: (string | number)[] = ["one", "two", "three", 4];
+// type aliases
+type stringOrNumber = string | number;
+type stringOrNumberArray = (string | number)[];
 
-let stringArr: (string | number)[];
-
-stringArr = ["one", "two", "three", 4];
-
-stringArr.push(5);
-stringArr.unshift("first");
-
-// console.log(stringArr)
-
-type KDramas = {
-  genre: string | boolean;
+type Guitarist = {
   name?: string;
-  year?: number;
-  good: boolean;
-  actors: string[];
+  active: boolean;
+  albums: stringOrNumberArray;
 };
 
-let Romance: KDramas = {
-  genre: "Romance",
-  name: "Queen of Tears",
-  year: 2024,
-  good: true,
-  actors: ["Kim Soo Hyun", "Kim Ji Won"],
-};
+type UserId = stringOrNumber
 
-let Psych: KDramas = {
-  genre: "Psychological Drama",
-  name: "It's okay not to be okay",
-//   year: 2020,
-  good: true,
-  actors: ["Kim Soo Hyun", "Seo Ye Ji"],
-};
-
-// Romance = Psych
-// console.log(Romance);
-// console.log(Psych);
-
-
-const greetActor = (kdrama: KDramas) => {
-   if(kdrama.name) {
-    return `Hello ${kdrama.name}`
-   }
-
-   return 'Hello'
-}
-
-console.log(greetActor(Psych))
