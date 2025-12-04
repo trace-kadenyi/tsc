b86b7a045110653d7e93f30e184d5115e25909ce
Tracey Kadenyi <treykadenyi@gmail.com>
2025-12-04 14:09:32 +0300

    custom type guard & never type

--- src/main.ts
+++ src/main.ts
@@ -108,3 +108,16 @@ const infinite = () => {
 };
 
 // logMsg(infinite());
+
+// custom type guard
+const isNumber = (value: any): boolean => {
+  return typeof value === "number" ? true : false;
+};
+
+// use of the never type
+const numberOrString = (value: string | number): string => {
+  if (typeof value === "string") return "string";
+  if (isNumber(value)) return "number";
+
+  return createError("This should NEVER happen");
+};
