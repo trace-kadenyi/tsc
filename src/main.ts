00928e3d3223be3418c60de726ad9d4b6f983ada
Tracey Kadenyi <treykadenyi@gmail.com>
2025-12-05 21:39:54 +0300

    interface

--- src/main.ts
+++ src/main.ts
@@ -33,10 +33,14 @@ class Coder {
   public getAge() {
     return `Hello, I'm ${this.age}.`;
   }
+
+  //   public getLang() {
+  //     return `I write ${this.lang}`;
+  //   }
 }
 
 const Trace = new Coder("Tracey", "Gospel", 33);
-console.log(Trace.getAge());
+// console.log(Trace.getAge());
 
 // extend Coder
 class WebDev extends Coder {
@@ -56,4 +60,29 @@ class WebDev extends Coder {
 }
 
 const Kadenyi = new WebDev("Lenovo", "Kadenyi", "Soul", 23);
-console.log(Kadenyi.getLang());
+// console.log(Kadenyi.getLang());
+
+/////////////////////////////    DIVIDER  ///////////////////////////////////////
+
+interface Musician {
+  name: string;
+  instrument: string;
+  play(action: string): string;
+}
+
+class Guitarist implements Musician {
+  name: string;
+  instrument: string;
+
+  constructor(name: string, instrument: string) {
+    this.name = name;
+    this.instrument = instrument;
+  }
+
+  play(action: string) {
+    return `${this.name} ${action} the ${this.instrument}.`;
+  }
+}
+
+const Page = new Guitarist("Jimmy", "guitar");
+console.log(Page.play("strums"));
