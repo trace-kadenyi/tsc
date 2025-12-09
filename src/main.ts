59103608d9f2a701d2a8356bb48e6330fed3dba4
Tracey Kadenyi <treykadenyi@gmail.com>
2025-12-09 20:18:30 +0300

    interface assignment

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
 // utility types
+interface Assignment {
+  studentId: string;
+  title: string;
+  grade: number;
+  verified?: boolean;
+}
+
+const updateAssignment = (
+  assign: Assignment,
+  propsToUpdated: Partial<Assignment>
+): Assignment => {
+  return { ...assign, ...propsToUpdated };
+};
+
+const assign1: Assignment = {
+  studentId: "compsci123",
+  title: "Final Project",
+  grade: 0,
+};
+
+console.log(updateAssignment(assign1, { grade: 95 }));
+
+
