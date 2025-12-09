3134a2095ae4b39d4f76a27b305dcf80accc28af
Tracey Kadenyi <treykadenyi@gmail.com>
2025-12-09 15:31:43 +0300

    isObj

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
 // generics
+const echo = <T>(arg: T): T => arg;
+
+const isObj = <T>(arg: T): boolean => {
+  return typeof arg === "object" && !Array.isArray(arg) && arg !== null;
+};
+
+console.log(isObj(true));
+console.log(isObj("john"));
+console.log(isObj([1, 2, 3]));
+console.log(isObj({ name: "John" }));
+console.log(isObj(null));
