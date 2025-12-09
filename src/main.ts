7f46bcb803c9edb0129f7770e58a19f8a74709fb
Tracey Kadenyi <treykadenyi@gmail.com>
2025-12-09 15:53:00 +0300

    interface boolcheck

--- src/main.ts
+++ src/main.ts
@@ -11,28 +11,59 @@ const isObj = <T>(arg: T): boolean => {
 // console.log(isObj({ name: "John" }));
 // console.log(isObj(null));
 
-const isTrue = <T>(arg: T): { arg: T; is: boolean } => {
+// const isTrue = <T>(arg: T): { arg: T; is: boolean } => {
+//   if (
+//     (Array.isArray(arg) && !arg.length) ||
+//     (isObj(arg) && !Object.keys(arg as keyof T).length)
+//   ) {
+//     return { arg, is: false };
+//   }
+
+//   return { arg, is: !!arg };
+// };
+
+// console.log(isTrue(false));
+// console.log(isTrue(0));
+// console.log(isTrue(true));
+// console.log(isTrue(1));
+// console.log(isTrue("dave"));
+// console.log(isTrue(""));
+// console.log(isTrue(null));
+// console.log(isTrue(undefined));
+// console.log(isTrue({}));
+// console.log(isTrue({ name: "Dave" }));
+// console.log(isTrue([]));
+// console.log(isTrue([1, 2, 3]));
+// console.log(isTrue(NaN));
+// console.log(isTrue(-0));
+
+interface BoolCheck<T> {
+  value: T;
+  is: boolean;
+}
+
+const checkBoolVlue = <T>(arg: T): BoolCheck<T> => {
   if (
     (Array.isArray(arg) && !arg.length) ||
     (isObj(arg) && !Object.keys(arg as keyof T).length)
   ) {
-    return { arg, is: false };
+    return { value: arg, is: false };
   }
 
-  return { arg, is: !!arg };
+  return { value: arg, is: !!arg };
 };
 
-console.log(isTrue(false));
-console.log(isTrue(0));
-console.log(isTrue(true));
-console.log(isTrue(1));
-console.log(isTrue("dave"));
-console.log(isTrue(""));
-console.log(isTrue(null));
-console.log(isTrue(undefined));
-console.log(isTrue({}));
-console.log(isTrue({ name: "Dave" }));
-console.log(isTrue([]));
-console.log(isTrue([1, 2, 3]));
-console.log(isTrue(NaN));
-console.log(isTrue(-0));
+console.log(checkBoolVlue(false));
+console.log(checkBoolVlue(0));
+console.log(checkBoolVlue(true));
+console.log(checkBoolVlue(1));
+console.log(checkBoolVlue("dave"));
+console.log(checkBoolVlue(""));
+console.log(checkBoolVlue(null));
+console.log(checkBoolVlue(undefined));
+console.log(checkBoolVlue({}));
+console.log(checkBoolVlue({ name: "Dave" }));
+console.log(checkBoolVlue([]));
+console.log(checkBoolVlue([1, 2, 3]));
+console.log(checkBoolVlue(NaN));
+console.log(checkBoolVlue(-0));
