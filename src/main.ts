94ed334f03a66173571525c3640da795769a5e89
Tracey Kadenyi <treykadenyi@gmail.com>
2025-12-09 15:57:11 +0300

    extend interface

--- src/main.ts
+++ src/main.ts
@@ -11,16 +11,16 @@ const isObj = <T>(arg: T): boolean => {
 // console.log(isObj({ name: "John" }));
 // console.log(isObj(null));
 
-// const isTrue = <T>(arg: T): { arg: T; is: boolean } => {
-//   if (
-//     (Array.isArray(arg) && !arg.length) ||
-//     (isObj(arg) && !Object.keys(arg as keyof T).length)
-//   ) {
-//     return { arg, is: false };
-//   }
+const isTrue = <T>(arg: T): { arg: T; is: boolean } => {
+  if (
+    (Array.isArray(arg) && !arg.length) ||
+    (isObj(arg) && !Object.keys(arg as keyof T).length)
+  ) {
+    return { arg, is: false };
+  }
 
-//   return { arg, is: !!arg };
-// };
+  return { arg, is: !!arg };
+};
 
 // console.log(isTrue(false));
 // console.log(isTrue(0));
@@ -53,17 +53,27 @@ const checkBoolVlue = <T>(arg: T): BoolCheck<T> => {
   return { value: arg, is: !!arg };
 };
 
-console.log(checkBoolVlue(false));
-console.log(checkBoolVlue(0));
-console.log(checkBoolVlue(true));
-console.log(checkBoolVlue(1));
-console.log(checkBoolVlue("dave"));
-console.log(checkBoolVlue(""));
-console.log(checkBoolVlue(null));
-console.log(checkBoolVlue(undefined));
-console.log(checkBoolVlue({}));
-console.log(checkBoolVlue({ name: "Dave" }));
-console.log(checkBoolVlue([]));
-console.log(checkBoolVlue([1, 2, 3]));
-console.log(checkBoolVlue(NaN));
-console.log(checkBoolVlue(-0));
+// console.log(checkBoolVlue(false));
+// console.log(checkBoolVlue(0));
+// console.log(checkBoolVlue(true));
+// console.log(checkBoolVlue(1));
+// console.log(checkBoolVlue("dave"));
+// console.log(checkBoolVlue(""));
+// console.log(checkBoolVlue(null));
+// console.log(checkBoolVlue(undefined));
+// console.log(checkBoolVlue({}));
+// console.log(checkBoolVlue({ name: "Dave" }));
+// console.log(checkBoolVlue([]));
+// console.log(checkBoolVlue([1, 2, 3]));
+// console.log(checkBoolVlue(NaN));
+// console.log(checkBoolVlue(-0));
+
+interface HasID {
+  id: number;
+}
+
+const processUser = <T extends HasID>(user: T): T => {
+  return user;
+};
+
+console.log(processUser({ id: 1, name: "Tracey" }));
