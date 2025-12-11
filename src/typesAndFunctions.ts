4496af7832c0996cbc805cb0ce55fceee691f529
Tracey Kadenyi <treykadenyi@gmail.com>
2025-12-11 12:35:51 +0300

    add func

--- src/typesAndFunctions.ts
+++ src/typesAndFunctions.ts
@@ -23,7 +23,7 @@ const add = (a: number, b: number): number => {
   return a + b;
 };
 
-// console.log(add(4, 5));
+console.log(add(4, 5));
 
 // no return
 const logMsg = (message: any): void => {
